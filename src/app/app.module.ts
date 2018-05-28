f8827fac1bf66cec5afcf1fe1d1e3c03dfa35ade
Pablo Crivello <pablocr_lu@lagash.com>
2018-05-27 22:14:31 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { AlumnosComponent } from './alumnos/alumnos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    LoginComponent,
+    AlumnosComponent
   ],
   imports: [
     BrowserModule
